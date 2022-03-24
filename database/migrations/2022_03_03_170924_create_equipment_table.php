bdfc640d83bd176c2535733305289891bf5ab992
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 14:01:50 -0700

    Update and rename 2022_03_03_170924_create_todos_table.php to 2022_03_03_170924_create_equipment_table.php

---
+++ database/migrations/2022_03_03_170924_create_equipment_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('equipment', function (Blueprint $table) {
+            $table->id();
+            $table->string('category');
+            $table->string('hardwareSpecs');
+            $table->integer('invoice');
+            $table->double('price' 20, 2);
+            $table->DateTime('purchaseDate');
+            $table->string('userName');
+            $table->string('userContact');
+            $table->string('manuName');
+            $table->string('saleContact');
+            $table->string('techContact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('equipments');
+    }
+};
