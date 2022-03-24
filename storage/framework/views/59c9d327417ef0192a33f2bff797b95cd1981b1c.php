24cae4e4e6ade32cc740b35ff949561de0aa8488
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 12:15:50 -0700

    Delete storage/framework directory

--- storage/framework/views/59c9d327417ef0192a33f2bff797b95cd1981b1c.php
+++
@@ -1,7 +0,0 @@
-<li <?php if(isset($item['id'])): ?> id="<?php echo e($item['id']); ?>" <?php endif; ?> class="nav-header <?php echo e($item['class'] ?? ''); ?>">
-
-    <?php echo e(is_string($item) ? $item : $item['header']); ?>
-
-
-</li>
-<?php /**PATH C:\Users\angel\cse4500-laravel-app\vendor\jeroennoten\laravel-adminlte\src/../resources/views/partials/sidebar/menu-item-header.blade.php ENDPATH**/ ?>
