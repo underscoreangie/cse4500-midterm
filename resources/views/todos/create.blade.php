0f8da3e8f1e530d30f3023569192e91ecdf66a03
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 13:50:42 -0700

    Update create.blade.php

--- resources/views/todos/create.blade.php
+++ resources/views/todos/create.blade.php
@@ -3,14 +3,19 @@
 @section('title', 'To Do List')
 
 @section('content_header')
-    <h1>To Do's</h1>
+    <h1>Creating Info</h1>
 @stop
 
 @section('content')
 <form method="post" action="{{ route('todos.store') }}" >
     @csrf
-    <x-adminlte-input name="title" label="Title" />
-    <x-adminlte-input name="progress" type="number" min=0 max=100 label="Progress" />
+    <x-adminlte-input name="title" label="Hardware Specs" />
+    <x-adminlte-input name="progress" type="number" min=0 max=1000 label="Invoice #" />
+    <x-adminlte-input name="progress" type="number" min=0 max=1000 label="Price" />
+    <x-adminlte-input name="progress" type="number" min=0 max=100 label="Purchase Date" />
+    <x-adminlte-input name="title" label="User Name" />
+    <x-adminlte-input name="title" label="Contact" />
+    <x-adminlte-input name="title" label="Notes History" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
