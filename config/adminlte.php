eb426a534a159f1c3c9f1bd47b1edd11fdfeaf54
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 13:37:26 -0700

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -240,11 +240,6 @@ return [
         'url'  => 'calendar',
         'icon' => 'fas fa-fw fa-calendar-alt',
     ],
-    [         
-        'text' => 'Board',
-        'url'  => 'board',
-        'icon' => 'fas fa-fw fa-board-alt',
-    ],
 ],
 
     /*
