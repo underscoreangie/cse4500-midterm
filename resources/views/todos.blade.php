83c869edcdd28b53dba624f7af4b7cf36cc4bc5f
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 12:43:12 -0700

    Create todos.blade.php

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,44 @@
+@extends('adminlte::page')
+
+@section('title', 'To Do List')
+
+@section('content_header')
+    <h1>To Do's</h1>
+@stop
+
+@section('content')
+<div class="card">
+  <div class="card-body">
+    <table id="table" class="table table-bordered">
+      <thead>
+        <tr>
+          <th style="width: 10px">#</th><th>Task</th><th>Progress</th><th style="width: 40px">Label</th>
+        </tr>
+      </thead>
+      <tbody>
+      
+       @foreach($todos AS $todo)
+        <tr>
+          <td>{{ $todo->id }}</td>
+          <td>{{ $todo->title }}</td>
+          <td><div class="progress progress-xs"><div class="progress-bar progress-bar-danger" style="width: {{ $todo->progress }}%"></div></div></td>
+          <td><a class="btn btn-default btn-sm" href="{{ route('todos.show',['todo'=>$todo->id]) }}">View</a></td>
+        </tr>
+        @endforeach
+        
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<a href="{{ route('todos.create') }} " class="btn btn-primary" >Create</a>
+
+@stop
+
+@section('js')
+<script>
+    $(document).ready(function() {
+        $('#table').DataTable();
+    } );
+</script>
+@stop
