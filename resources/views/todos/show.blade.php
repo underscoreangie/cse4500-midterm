b958123fc0a57fc4042ef09061e4cdf251b9789a
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 14:23:18 -0700

    Update show.blade.php

--- resources/views/todos/show.blade.php
+++ resources/views/todos/show.blade.php
@@ -1,12 +1,13 @@
 @extends('adminlte::page')
 
-@section('title', 'To Do List')
+@section('title', 'Equipment')
 
 @section('content_header')
-    <h1>To Do's</h1>
+    <h1>Equipment</h1>
 @stop
 
 @section('content')
-  <h2>{{ $todo->title; }}</h2>
-  <div><p>{{ $todo->progress; }}% finished</p></div>
+  <h2>{{ $equipment->hardwareSpecs; }}</h2>
+  <div><p>{{ $equipment->userName; }}% finished</p></div>
+  <h2>{{ $equipment->inovice; }}</h2>
 @stop
