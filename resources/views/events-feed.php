b79f1f0a366f552a0b3504ffb2d180ca15d64277
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-24 12:43:58 -0700

    Create events-feed.php

---
+++ resources/views/events-feed.php
@@ -0,0 +1,18 @@
+{
+    $events = array([first],[second]);
+
+    echo json_encode($events);
+}
+
+[
+    {
+      "title": "CSE4500 Class",
+      "start": "2022-02-23T17:30:00",
+      "end": "2022-02-23T18:45:00"
+    },
+    {
+      "title": "CSE4500 Class",
+      "start": "2022-02-28T17:30:00",
+      "end": "2022-02-28T18:45:00"
+    },   
+  ]
