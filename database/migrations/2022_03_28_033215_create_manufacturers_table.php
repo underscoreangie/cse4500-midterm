3aa186b0cc96ac90e47db0539c2b2e166e3e5514
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-29 13:33:37 -0700

    Create 2022_03_28_033215_create_manufacturers_table.php

---
+++ database/migrations/2022_03_28_033215_create_manufacturers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('manufacturers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string("name");
+            $table->string("tech_email");
+            $table->string("tech_phone");
+            $table->string("sales_email");
+            $table->string("sales_phone");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('manufacturers');
+    }
+};
