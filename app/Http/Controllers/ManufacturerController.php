20defb650cac5b22617f765c39198a089a3ac8b3
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-29 13:38:05 -0700

    Create ManufacturerController.php

---
+++ app/Http/Controllers/ManufacturerController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Manufacturer;
+use Kris\LaravelFormBuilder\FormBuilder;
+use App\Forms\ManufacturerForm;
+
+class ManufacturerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $manufacturer = Manufacturer::all();
+        return json_encode(compact('manufacturer'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create(FormBuilder $formBuilder)
+    {
+        $form = $formBuilder->create(ManufacturerForm::class, [
+            'method' => 'POST',
+            'url' => route('manufacturer.store')
+        ]);
+        return view('manufacturer.create', compact('form'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(FormBuilder $formBuilder)
+    {
+        $form = $formBuilder->create(ManufacturerForm::class);
+        $form->redirectIfNotValid();
+        Manufacturer::create($form->getFieldValues());
+        return $this->index();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
