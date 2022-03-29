cfc2b14a71f397c32c3531fa0c2dcd2d125068ed
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-29 11:37:17 -0700

    Update create.blade.php

--- resources/views/equipments/create.blade.php
+++ resources/views/equipments/create.blade.php
@@ -18,7 +18,7 @@
     <x-adminlte-input name="title" label="Contact" />
     <x-adminlte-input name="progress" type="number" min=0 max=1000 label="Invoice #" />
     <x-adminlte-input name="progress" type="number" min=0 max=1000 label="Price" />
-    <x-adminlte-input name="progress" type="number" min=0 max=100 label="Purchase Date" />
+    <x-adminlte-input name="start" type="datetime-local" label="Start" label="Purchase Date" />
     <x-adminlte-button type="Submit" label="Submit" />
 </form>
 @stop
