431abcc880dcc1ded94424c53b413971b3b5d616
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-29 20:57:07 -0700

    Update EquipmentController.php

--- app/Http/Controllers/EquipmentController.php
+++ app/Http/Controllers/EquipmentController.php
@@ -4,91 +4,58 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Equipment;
-use Kris\LaravelFormBuilder\FormBuilder;
-use App\Forms\EquipmentForm;
-
 
 class EquipmentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
     public function index()
     {
-        $equipment = Equipment::all();
-        return json_encode(compact('equipment'));
+        $equipments = Equipment::all();
+        return view('equipments',compact('equipments'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create(FormBuilder $formBuilder)
+
+    public function create()
     {
-        $form = $formBuilder->create(EquipmentForm::class, [
-            'method' => 'POST',
-            'url' => route('equipment.store')
-        ]);
-        return view('equipment.create', compact('form'));
+        return view('equipments.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(FormBuilder $formBuilder)
+
+    public function store(Request $request)
     {
-        $form = $formBuilder->create(EquipmentForm::class);
-        $form->redirectIfNotValid();
-        Equipment::create($form->getFieldValues());
-        return $this->index();
+        $validated = $request->validate([
+                'title' => 'required',
+                'progress' => 'required',
+         ]);
+      
+         $equipment = Equipment::create([
+                'title' => $request->title,
+                'progress' => $request->progress, 
+         ]);
+      
+          return $this->index();
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function show($id)
     {
-        //
+          $equipment= Equipment::find($id);
+          return view('equipments.show',compact('equipment'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function edit($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+
     public function destroy($id)
     {
         //
