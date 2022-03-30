6f99de1309fd2be51db65a250aa2b511e16ab479
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-30 12:17:24 -0700

    Update app.php

--- config/app.php
+++ config/app.php
@@ -177,7 +177,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         
-         Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
 
     ],
 
