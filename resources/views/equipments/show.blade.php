8a5fd6eff1f78f83507ab85c2b0d71e178d8ae28
underscoreangie <78881883+underscoreangie@users.noreply.github.com>
2022-03-30 16:55:11 -0700

    Update show.blade.php

--- resources/views/equipments/show.blade.php
+++ resources/views/equipments/show.blade.php
@@ -7,7 +7,15 @@
 @stop
 
 @section('content')
+  <h2>{{ $equipment->category; }}</h2>
   <h2>{{ $equipment->hardwareSpecs; }}</h2>
-  <div><p>{{ $equipment->userName; }}% finished</p></div>
+  <h2>{{ $equipment->manufacture; }}</h2>
+  <h2>{{ $equipment->saleInfo; }}</h2>
+  <h2>{{ $equipment->contactInfo; }}</h2>
+  <h2>{{ $equipment->userName; }}</h2>
+  <h2>{{ $equipment->contact; }}</h2>
+  <h2>{{ $equipment->invoice; }}</h2>
+  <h2>{{ $equipment->price; }}</h2>
+  <h2>{{ $equipment->purchaseDate; }}</h2>
   <h2>{{ $equipment->inovice; }}</h2>
 @stop
